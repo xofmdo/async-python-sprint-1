5c321a9105dbf28f4e5649a8bd59ed499578de46
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2023-01-11 23:15:22 +0300

    async solution

--- forecasting.py
+++ forecasting.py
@@ -1,9 +1,9 @@
 # import logging
-# import threading
-# import subprocess
-# import multiprocessing
 
-from api_client import YandexWeatherAPI
+import concurrent
+from concurrent.futures import ThreadPoolExecutor
+
+from models import CityModel
 from tasks import (
     DataFetchingTask,
     DataCalculationTask,
@@ -11,38 +11,38 @@ from tasks import (
     DataAnalyzingTask,
 )
 from utils import CITIES
-from models import (
-    CityModel, DayWeatherConditionsModel,
-    CombinedWeatherConditionsModel, FinalOutputModel
-)
 
 
-def forecast_weather() -> list[CityModel]:
-    """
-    Анализ погодных условий по городам
-    """
-    forecating = []
-    for city in CITIES.keys():
-        city_model = DataFetchingTask().get_data(city_name=city)
-        forecating.append(city_model)
+# можно было реализовать вычисление результатов по городам через Queue, но
+# преимуществ у данного способа перед ProcessPoolExecutor() не увидел
 
-    return forecating
+def forecast_weather(forecasts: list[CityModel]) -> list[dict]:
+    class_for_calculations = DataCalculationTask()
 
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        data = executor.map(
+            class_for_calculations.general_calculation,
+            forecasts
+        )
 
-def calculatinfg_value(data: list[CityModel]) -> list[dict]:
-    result = []
-    calculating_class = DataCalculationTask()
-    for city in data:
-        calculating_data = calculating_class.calculating_data(city)
-        calculating = calculating_class.calc_general_indicators(calculating_data)
-        result.append(calculating)
+    result_data = class_for_calculations.adding_rating(list(data))
 
-    total = calculating_class.adding_rating(result)
-    return total
+    return result_data
 
 
 if __name__ == "__main__":
-    tmp = calculatinfg_value(forecast_weather())
-    saving_process = DataAggregationTask().save_results(tmp)
-    result = DataAnalyzingTask().get_result(tmp)
-    print(result)
+    class_for_analysis = DataAnalyzingTask()
+
+    with ThreadPoolExecutor() as pool:
+        results = pool.map(
+            DataFetchingTask().get_data,
+            CITIES.keys()
+        )
+
+    forecast_responses = list(results)
+    result = forecast_weather(forecast_responses)
+
+    DataAggregationTask().save_results(result)
+
+    results_analysis = class_for_analysis.get_result(result)
+    print(results_analysis)
--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 import json
-import pprint
+
 from api_client import YandexWeatherAPI
 from models import (
     CityModel, DayWeatherConditionsModel,
@@ -29,16 +29,18 @@ class DataCalculationTask:
     """
 
     @staticmethod
-    def additional_calculating(temp_data: list) -> float:
-        """ Калькулирование средних показателей с округлением."""
+    def __additional_calculating(temp_data: list) -> float:
+        """ Вычисление средних показателей с округлением."""
+
         if temp_data:
             return round(sum(temp_data) / len(temp_data), 2)
         else:
             return 0
 
-    def calculating_data(self,
-                         city_data: CityModel) -> CombinedWeatherConditionsModel:
+    def __calculating_data(self,
+                           city_data: CityModel) -> CombinedWeatherConditionsModel:
         """ Вычисление средних показателей"""
+
         weather_data = []
         for day in city_data.forecasts.forecasts:
             result_temp_for_day = []
@@ -53,7 +55,7 @@ class DataCalculationTask:
             weather_data.append(
                 DayWeatherConditionsModel(
                     date=day.date,
-                    daily_avg_temp=self.additional_calculating(
+                    daily_avg_temp=self.__additional_calculating(
                         result_temp_for_day),
                     clear_weather_cond=count_clear_cond
                 ))
@@ -63,10 +65,12 @@ class DataCalculationTask:
             data=weather_data,
         )
 
-    def calc_general_indicators(self,
-                                data: CombinedWeatherConditionsModel) -> \
+    def __calc_general_indicators_for_day(self,
+                                          data:
+                                          CombinedWeatherConditionsModel) -> \
             FinalOutputModel:
         """Добавление финальных показателей"""
+
         list_avg_temp = []
         total_avg_clear_weather_cond = 0
         for element in data.data:
@@ -74,7 +78,7 @@ class DataCalculationTask:
                 list_avg_temp.append(element.daily_avg_temp)
                 total_avg_clear_weather_cond += element.clear_weather_cond
 
-        total_avg_temperature = self.additional_calculating(list_avg_temp)
+        total_avg_temperature = self.__additional_calculating(list_avg_temp)
         return FinalOutputModel(
             city=data.city,
             data=data.data,
@@ -83,9 +87,18 @@ class DataCalculationTask:
             rating=0
         )
 
+    def general_calculation(self,
+                            city_data: CityModel) -> FinalOutputModel:
+        """Вычисление результатов для обработки вне класса"""
+
+        calculating = self.__calculating_data(city_data)
+        result = self.__calc_general_indicators_for_day(calculating)
+        return result
+
     @staticmethod
     def adding_rating(data: list[FinalOutputModel]) -> list[dict]:
         """Добавление рейтинга города"""
+
         total_rating = [
             (one_model.city,
              one_model.total_avg_temp,
@@ -110,6 +123,7 @@ class DataAggregationTask:
     @staticmethod
     def save_results(to_save: list[dict]) -> None:
         """Сохранение аналитических данных"""
+
         with open(RESULT_FILE_NAME, 'w', encoding='utf-8') as file:
             json.dump(to_save, file, indent=2)
 
@@ -122,6 +136,7 @@ class DataAnalyzingTask:
     @staticmethod
     def get_result(data: list[dict]) -> str:
         """Получение результата"""
+
         result = []
         max_temp = -9999
         sunniest_weather = 0
@@ -144,11 +159,12 @@ class DataAnalyzingTask:
                             city_data['total_clear_weather_cond']
                     }
                 )
-        answer = 'Самые лучшие погодные условия у нас получились в городе'
+        answer = 'Самые лучшие погодные условия получились в городе'
         result_str = ''
         for city in result:
             result_str += f' {city["city"]}, средняя температура: ' \
-                          f'{city["total_avg_temp"]}, а количество времени без ' \
-                          f'осадков {city["total_clear_weather_cond"]}часов.\n'
+                          f'{city["total_avg_temp"]}, а количество ' \
+                          f'времени без осадков {city["total_clear_weather_cond"]} ' \
+                          f'часов.\n'
 
         return answer + result_str
